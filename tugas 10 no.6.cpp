b01da578df67ded1598cebe9e1af812d4749f7d1
gilang-w <61899269+gilang-w@users.noreply.github.com>
2020-06-10 10:39:05 +0800

    Add files via upload

---
+++ tugas 10 no.6.cpp
@@ -0,0 +1,15 @@
+6#include <iostream>
+using namespace std;
+#include "process.h"
+
+int main(){
+    
+    ADD(10, 15);
+    
+    MULTIPLY(10, 15);
+    
+    cout<<"Proses Telah Selesai";
+}
+
+
+
